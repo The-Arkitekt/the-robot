ecc24104bfaa0bbb4816ad462a9ab24e55201e19
The-Arkitekt <mr.davidcrumley@gmail.com>
2024-05-16 17:09:10 +0000

    updated Container file nd sync-project

--- scripts/sync-project.sh
+++ scripts/sync-project.sh
@@ -4,3 +4,8 @@
 # WARNING: Replaces all contents of current source directory
 
 podman cp robot-dev:/home/development-user/source $PWD/..
+
+# Pull the design directory from the development environment container
+# WARNING: Replaces all contents of current source directory
+
+podman cp robot-dev:/home/development-user/design $PWD/..
